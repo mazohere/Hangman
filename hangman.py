f5da21028cfb61494018b85599587b2b0b93619f
mazo_ <83385567+mazohere@users.noreply.github.com>
2021-11-18 12:40:45 +1100

    some more (not much more)

--- hangman.py
+++ hangman.py
@@ -1,3 +1,5 @@
+import random
+
 hangman_images = ['''
   +---+
   |   |
@@ -48,3 +50,33 @@ hangman_images = ['''
  / \  |
       |
 =========''']
+
+def choice():
+
+    level_1_get = open("level_1.txt").read().split()
+
+    level_1_word = random.choice(level_1_get)
+
+    level_2_get = open("level_2.txt").read().split()
+
+    level_2_word = random.choice(level_2_get)
+
+    level_3_get = open("level_3.txt").read().split()
+
+    level_3_word = random.choice(level_3_get)
+
+    level_4_get = open("level_4.txt").read().split()
+
+    level_4_word = random.choice(level_4_get)
+
+    level_5_get = open("level_5.txt").read().split()
+
+    level_5_word = random.choice(level_5_get)
+
+    print(level_1_word)
+    print(level_2_word)
+    print(level_3_word)
+    print(level_4_word)
+    print(level_5_word)
+
+choice()
